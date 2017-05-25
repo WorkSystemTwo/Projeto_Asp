e01a3507672657dc7bc1a825d95f001ed2a98185
WorkSystemTwo <leitema2011@hotmail.com>
2017-05-25 19:27:06 -0300

    alteração nome botao

--- Projeto_Asp/cadastrarCliente.aspx.designer.cs
+++ Projeto_Asp/cadastrarCliente.aspx.designer.cs
@@ -112,31 +112,31 @@ namespace Projeto_Asp {
         protected global::System.Web.UI.WebControls.TextBox txtObs;
         
         /// <summary>
-        /// Button1 control.
+        /// btnNovo control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button btnNovo;
         
         /// <summary>
-        /// Button2 control.
+        /// btnSalvar control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button btnSalvar;
         
         /// <summary>
-        /// Button3 control.
+        /// btnCancelar control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button3;
+        protected global::System.Web.UI.WebControls.Button btnCancelar;
         
         /// <summary>
         /// Label6 control.
