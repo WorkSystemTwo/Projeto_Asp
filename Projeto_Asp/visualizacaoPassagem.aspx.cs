d998c395df782980c4a36d6e36ac9779ad68bc26
WorkSystemTwo <leitema2011@hotmail.com>
2017-05-25 21:09:09 -0300

    Consulta Passagem

--- Projeto_Asp/visualizacaoPassagem.aspx.cs
+++ Projeto_Asp/visualizacaoPassagem.aspx.cs
@@ -7,34 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Projeto_Asp
 {
-    public partial class cadastroPassagem : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class visualizacaoPassagem : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-
-        protected void btnEditar_Click(object sender, EventArgs e)
-        {
-            txtCodPassagem.Enabled = txtCodViagem.Enabled = txtCpf.Enabled = txtPoltrona.Enabled = true;
-        }
-
-        protected void btnCancelar_Click(object sender, EventArgs e)
-        {
-            Limpar();
-            desabilitar();
-        }
-        private void Limpar()
-        {
-            txtCodPassagem.Text = txtCodViagem.Text = txtCpf.Text = txtPoltrona.Text = "";
-        }
-        private void desabilitar()
-        {
-            txtCodPassagem.Enabled = txtCodViagem.Enabled = txtCpf.Enabled = txtPoltrona.Enabled = false;
-        }
-        private void Habilitar()
-        {
-            txtCodPassagem.Enabled = txtCodViagem.Enabled = txtCpf.Enabled = txtPoltrona.Enabled = false;
-        }
-    }
+		}
+	}
 }
